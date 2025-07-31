886f5500ad9953ec03c520331dc20659320713f8
Jady Lima <jadylsilva@gmail.com>
2025-07-31 16:35:07 -0300

    refactor: Mensagens de validação dos dados do formulário.
    Adicionado novas mensagens de validação dos dados do formulário.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,10 +13,19 @@ class AuthController extends Controller
 
     public function loginSubmit(Request $request)
     {
-        $request->validate([
-            'text_username' => 'required',
-            'text_password' => 'required'
-        ]);
+        $request->validate(
+            [
+                'text_username' => 'required|email',
+                'text_password' => 'required|min:6|max:16'
+            ],
+            [
+                'text_username.required' => 'Oops! Username is required. Please enter a valid email address.',
+                'text_username.email' => 'Oops, the username must be a valid email. Try again!',
+                'text_password.required' => 'Oops! Password is required.',
+                'text_password.min' => '"Oops! Password can have a minimum of :min characters',
+                'text_password.max' => '"Oops! Password can have a maximum of :max  characters',
+            ]
+        );
 
         $username = $request->input('text_username');
         $password = $request->input('text_password');
