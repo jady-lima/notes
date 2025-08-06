98b8bc1b72fc7e27d993414703cbfbd8852a542e
Jady Lima <jadylsilva@gmail.com>
2025-08-06 11:52:52 -0300

    feat: Edit e delete routes
    Novas rotas para editar e deletar uma nota

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,17 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 
 class MainController extends Controller
 {
     public function index()
     {
-        return view("home");
+        $id = session('user.id');
+
+        $notes = User::find($id)
+                    ->notes()
+                    ->get();
+
+        return view("home", compact('notes'));
     }
 
     public function newNote()
     {
         echo "I'm creating a new note";
+    }
+
+    public function editNote($id)
+    {
+        echo "Editing a note: ". Crypt::decrypt($id);
+    }
+
+    public function deleteNote($id)
+    {
+        echo "Deleting a note: ". Crypt::decrypt($id);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,11 @@ Route::middleware([CheckIsNotLogged::class])->group(function () {
 
 Route::middleware([CheckIsLogged::class])->group(function () {
     Route::get('/', [MainController::class, 'index'])->name('index');
+
     Route::get('/newnote', [MainController::class, 'newNote'])->name('newnote');
+    Route::get('/edit/{id}', [MainController::class, 'editNote'])->name('editnote');
+    Route::get('/delete/{id}', [MainController::class, 'deleteNote'])->name('deletenote');
+
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 });
     
